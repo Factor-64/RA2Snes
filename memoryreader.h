b9896f9c7a310129116c5f9dd92ca9b5537097aa
Factor-64 <101234202+Factor-64@users.noreply.github.com>
2025-07-20 00:12:34 -0400

    Cleaned up code and removed memoryreader thread

--- memoryreader.h
+++ memoryreader.h
@@ -18,9 +18,7 @@ public:
     void initTriggers(const QList<AchievementInfo>& achievements, const QList<LeaderboardInfo>& leaderboards, const QString& richPresence, const unsigned int& ramSize);
     void remapTriggerAddresses(bool modified);
     QList<QPair<unsigned int, unsigned int>> getUniqueMemoryAddresses();
-    //void checkLeaderboards();
-    void addFrameToQueues(const QByteArray& data, const unsigned int& frames);
-    void clearQueue();
+    void processFrames(const char* data, unsigned int& frames);
 
 signals:
     void finishedMemorySetup();
@@ -29,27 +27,18 @@ signals:
     void leaderboardsChecked();
     void leaderboardCompleted(const unsigned int& id, const QDateTime& time);
     void updateAchievementInfo(const unsigned int& id, const AchievementInfoType& infotype, const int& value);
-    void modifiedAddresses(const QList<QPair<unsigned int, unsigned int>> uma);
+    void modifiedAddresses();
     void updateRichPresence(const QString& status);
-    void continueQueue();
 
 private:
     void decrementAddressCounts(rc_memrefs_t& memrefs);
-    void checkAchievements();
-    void runQueue();
-    void processFrames();
     QList<QPair<unsigned int, unsigned int>> uniqueMemoryAddresses;
     QMap<unsigned int, rc_trigger_with_memrefs_t*> achievementTriggers;
-    QMap<unsigned int, rc_lboard_t*> leaderboardTriggers;
-    QQueue<QPair<QByteArray, int>> frameQueue;
+    //QMap<unsigned int, rc_lboard_t*> leaderboardTriggers;
     QMap<unsigned int, unsigned int> uniqueMemoryAddressesCounts;
     QMap<unsigned int, unsigned int> addressMap;
     rc_richpresence_with_memrefs_t* mem_richpresence;
-    QMutex frameQueueMutex;
-    QMutex triggersMutex;
-    QMutex uniqueAddressMutex;
-    QAtomicInt rpState;
-    //QQueue<QPair<QByteArray, int>> leaderboardFrames;
+    int rpState;
 };
 
 
