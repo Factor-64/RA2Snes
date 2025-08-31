031c096e2148606d55a2cdbdaf37579fc71de880
Factor-64 <101234202+Factor-64@users.noreply.github.com>
2025-08-31 15:48:26 -0400

    Fixed achievement url comparison

--- achievementmodel.h
+++ achievementmodel.h
@@ -54,7 +54,7 @@ public:
 
 signals:
     void unlockedChanged(const unsigned int& index);
-    void primedChanged(const QUrl& badgeUrl);
+    void primedChanged(const QUrl& badgeUrl, const bool& p);
     void valueChanged(const QUrl& badgeUrl, const unsigned int& value, const unsigned int& total);
 
 private:
